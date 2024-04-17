eb46bd9dc101a8685022fa3868fdb22b1e502f2d
nitinsharma42 <nitinsharma210691@gmail.com>
2024-04-17 10:33:10 +0530

    commit

--- src/components/modals/EditProfile/EditProfile.tsx
+++ src/components/modals/EditProfile/EditProfile.tsx
@@ -1,8 +1,7 @@
 import { useContext } from "react";
-import { Formik, Form, ErrorMessage, FormikProps, Field } from "formik";
+import { Formik, ErrorMessage, FormikProps } from "formik";
 import * as Yup from "yup";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Typography from "@mui/material/Typography";
